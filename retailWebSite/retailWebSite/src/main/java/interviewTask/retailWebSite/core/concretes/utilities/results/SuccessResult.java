e4866b64a9fd54f516fd0237abbbe5060def3b8a
ByrMucahit <m.mucahitbayar@gmail.com>
2021-12-06 16:36:47 +0300

    Quber

---
+++ retailWebSite/retailWebSite/src/main/java/interviewTask/retailWebSite/core/concretes/utilities/results/SuccessResult.java
@@ -0,0 +1,31 @@
+package interviewTask.retailWebSite.core.concretes.utilities.results;
+
+
+public class SuccessResult extends Result {
+
+	/* Constructor 1 */
+	public SuccessResult() {
+		super(true);
+	}
+	
+	/* Constructor 2 */
+	public SuccessResult(String message) {
+		super(true, message);
+	}
+}
+
+
+/*
+ * Description:
+ * -------------
+ * purpose of this class's  data controller successfully. It's derivated by superclass named Result.
+ * 
+ * Parameters:
+ * -------------
+ * It has same parameters with own superclass.
+ * 
+ * Return:
+ * -------------
+ * None
+ *  */
+ 
---
+++ retailWebSite/retailWebSite/src/test/java/interviewTask/retailWebSite/RetailWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package interviewTask.retailWebSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
